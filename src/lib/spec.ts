9f170a679508461974478559f440ca2c97782bb1
Ongsalt <ongsa.srp.mkm@gmail.com>
2026-03-15 03:16:11 +0700

    init

---
+++ src/lib/spec.ts
@@ -0,0 +1,24 @@
+interface Result {
+  score: number;
+  scoreRank: string; // its C B A S or so i dont remember, so just make this a string
+  highScore: number;
+  perfect: number;
+  great: number;
+  good: number;
+  bad: number;
+  miss: number;
+  maxCombo: number;
+
+  detail?: {
+    late: number;
+    early: number; // in jp this say "fast"
+    wrongWay: number; // in jp this say "flick: ${number}"
+  };
+
+  song: {
+    name: string;
+    difficulty: "easy" | "normal" | "hard" | "master" | "append";
+    level: number;
+  };
+}
+
